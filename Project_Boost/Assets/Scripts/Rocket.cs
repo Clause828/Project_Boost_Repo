9ce23e74fb29cbcb11775832d0c2cc93d0981a30
Jayden Stipek <55769079+Clause828@users.noreply.github.com>
2020-07-28 07:19:22 -0700

    Adding Sound and Movement to Rocket

--- Project_Boost/Assets/Scripts/Rocket.cs
+++ Project_Boost/Assets/Scripts/Rocket.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,15 +22,21 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Thrusters Enabled");
+            rigidbody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if(Input.GetKey(KeyCode.A))
         {
-            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
+            transform.Rotate(Vector3.back);
         }
     }
 }
